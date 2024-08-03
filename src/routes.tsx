2ca41506a8d29030117322dda36b50d2e9fa2efd
shahrad kashani <shahrad.kashani@wallit.eu>
2024-08-04 01:08:28 +0330

    Feat(route): add layout to route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import {Home} from "@components/pages/home";
 import { Details } from "@components/pages/details";
 import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "@components/templates/layout";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Layout title="home"><Home /></Layout>,
     },
     {
       path: "/details/:id",
-      element: <Details />,
+      element: <Layout title="Details"><Details /></Layout>,
     }
   ]);
