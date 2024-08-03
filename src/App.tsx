0e6cfcc41a756d917b611175bbc8c56fa6004f3f
shahrad kashani <shahrad.kashani@wallit.eu>
2024-08-04 01:10:24 +0330

    Feat(app): remove layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import * as React from "react";
 import { Provider as ReduxProvider } from 'react-redux';
 import "./css/styles.scss";
 import { store } from "./redux/store";
-import { Layout } from "@components/templates/layout";
 import {RouterProvider} from "react-router-dom";
 import { router } from "./routes";
 
 const App = () => {
-
     return <ReduxProvider store={store}>
-                    <Layout title="home">
-                        <RouterProvider router={router} />
-                    </Layout>
+                <RouterProvider router={router} />
         </ReduxProvider>
 }
 
