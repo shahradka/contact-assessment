c938d83de932cc0f8371121df5e182fdb6967b2b
shahrad kashani <shahrad.kashani@wallit.eu>
2024-08-03 10:56:16 +0330

    Feat(Test): Delete Test Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import * as React from "react";
 import { Provider } from 'react-redux';
 import "./css/styles.scss";
 import { store } from "./redux/store";
-import {Test} from "@components/atoms/test";
+import {Text} from '@components/atoms/text';
 
 const App = () => {
 
     return <Provider store={store}>
                 <div>
                     Contact App  Assessment!
-                    <Test />
                 </div>    
         </Provider>
 }
