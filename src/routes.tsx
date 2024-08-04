95995c0a378d0eca7baaaaec88379de4323ff65e
shahrad kashani <shahrad.kashani@wallit.eu>
2024-08-04 14:20:31 +0330

    Feat(route): add contact record component

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import {Home} from "@components/pages/home";
 import { Details } from "@components/pages/details";
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "@components/templates/layout";
+import { ContactRecords } from "@components/pages/contactRecords";
+
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout title="home"><Home /></Layout>,
+      element: <Layout title="home" contactRecordsComp={<ContactRecords />}><Home /></Layout>,
     },
     {
       path: "/details/:id",
